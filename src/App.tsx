5e86e6f121ec1e544e85c7a3573fbd9ee5d629f4
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-24 13:39:06 -0600

    Agregando estilo a los componentes con inline

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,32 @@ interface AppProps {
     estado?: string;
 }
 
+const estilo = ({ bg = '#222' }) => ({
+    backgroundColor: bg,
+    color: '#eee',
+    padding: '10px 15px',
+    margin: '10px 15px',
+});
+const estilo2 = {
+    boxShadow: '2px 5px 3px rgba(0, 0, 0, 0.5)',
+};
+
 const Li: React.FC<AppProps> = ({ children, estado }) => {
     console.log(estado);
-    return <li className="clase-li">{children}</li>;
+
+    return (
+        <li
+            style={{ ...estilo2, ...estilo({ bg: '#333' }) }}
+            className="clase-li"
+        >
+            {children}
+        </li>
+    );
 };
 
 const App = () => {
     return (
-        <ul className="clase-css">
+        <ul style={estilo({ bg: '#750' })} className="clase-css">
             <Li estado="feliz">valor de li</Li>
         </ul>
     );
