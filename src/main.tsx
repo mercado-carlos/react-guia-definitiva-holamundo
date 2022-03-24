80433909e4dbe296468338d2c942a787771e700b
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-24 12:58:05 -0600

    Componentes y propiedades

--- src/main.tsx
+++ src/main.tsx
@@ -10,5 +10,29 @@ import App from './App';
   document.getElementById('root')
 ) */
 
-const Div = () => <p>Hola Mundo</p>;
-ReactDOM.render(<Div />, document.getElementById('root'));
+interface MainProps {
+    estado?: string;
+    casa?: boolean;
+    edad?: number;
+}
+
+const Li: React.FC<MainProps> = ({ children, estado, casa, edad }) => {
+    console.log(casa, edad);
+    return (
+        <li>
+            {children} estoy {estado}
+        </li>
+    );
+};
+
+const Render = () => (
+    <ul>
+        <Li estado={'feliz'} casa={false} edad={24}>
+            Chanchito
+        </Li>
+        <Li estado={'triste'}>Chanchito</Li>
+        <Li estado={'emocionado'}>Carlos</Li>
+    </ul>
+);
+
+ReactDOM.render(<Render />, document.getElementById('root'));
