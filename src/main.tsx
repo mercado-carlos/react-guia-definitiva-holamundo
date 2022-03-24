8f39f422ac57ba23ad9268b1f9f5ca9f5a2c2a90
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-24 12:34:33 -0600

    Introduccion a TSX

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import './index.css';
+import App from './App';
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-)
+) */
+
+const Div = () => <p>Hola Mundo</p>;
+ReactDOM.render(<Div />, document.getElementById('root'));
