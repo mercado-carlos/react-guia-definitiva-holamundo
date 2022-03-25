8571c389d5fabdf7f6503032ddfce94a61ac5818
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-25 12:31:24 -0600

    Estado inicial y cargando imagenes

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,22 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends React.Component {
+    state = {
+        productos: [
+            { name: 'Tomate', price: 1500, img: '/img/tomate.jpg' },
+            { name: 'Chicharo', price: 1500, img: '/img/chicharo.jpg' },
+            { name: 'Lechuga', price: 1500, img: '/img/lechuga.jpg' },
+        ],
+    };
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    render() {
+        return (
+            <div>
+                <p>Hola Mundo</p>
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
