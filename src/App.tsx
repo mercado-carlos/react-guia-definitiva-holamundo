faeeade2c048f511b29a5ebf1505babc447c4564
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-25 08:28:53 -0600

    Class State

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
+    state = {
+        valor: 3,
+    };
+
     render() {
-        return <p>Hola mundo</p>;
+        console.log(this.state);
+
+        return (
+            <div>
+                <p>Hola mundo</p>
+                <button
+                    className={`${this.state.valor}`}
+                    onClick={() => this.setState({ valor: 2 })}
+                >
+                    Enviar
+                </button>
+            </div>
+        );
     }
 }
 
