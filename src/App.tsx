2f7dc305808f46a46bff8e97bef77d9ed386502e
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-28 16:07:09 -0600

    Custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 
-class App extends Component {
-    state = { contador: 0 };
-
-    incrementar = () => {
-        this.setState({ contador: this.state.contador + 1 });
+const useContador = (inicial: any) => {
+    const [contador, setContador] = useState(inicial);
+    const incrementar = () => {
+        setContador(contador + 1);
     };
+    return [contador, incrementar];
+};
 
-    render() {
-        return (
-            <div>
-                Contador: {this.state.contador}
-                <button onClick={this.incrementar}>Incrementar</button>
-            </div>
-        );
-    }
-}
-
-/* const App = () => {
-    const [contador, setContador] = useState(0);
-
+const App = () => {
+    const [contador, incrementar] = useContador(0);
     return (
         <div>
             Contador: {contador}
-            <button onClick={() => setContador(contador + 1)}>
-                Incrementar
-            </button>
+            <button onClick={incrementar}>Incrementar</button>
         </div>
     );
-}; */
+};
 
 export default App;
