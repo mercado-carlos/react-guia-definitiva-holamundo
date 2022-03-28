db2d6f82a6827ab7dc9a43cc9d2d3ceedd8aeb39
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-28 16:15:19 -0600

    useEffect

--- src/useState.tsx
+++ src/useState.tsx
@@ -1,22 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const useContador = (inicial: any) => {
     const [contador, setContador] = useState(inicial);
-
     const incrementar = () => {
         setContador(contador + 1);
     };
-
     return [contador, incrementar];
 };
 
 const App = () => {
     const [contador, incrementar] = useContador(0);
-
-    useEffect(() => {
-        document.title = contador;
-    }, [contador]);
-
     return (
         <div>
             Contador: {contador}
