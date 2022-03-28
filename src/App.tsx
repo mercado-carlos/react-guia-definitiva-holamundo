7450e32fba3f2c316c86b029450cde509d8e3743
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-28 16:34:49 -0600

    useEffect basado en clases

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 
 const useContador = (inicial: any) => {
     const [contador, setContador] = useState(inicial);
@@ -10,16 +10,42 @@ const useContador = (inicial: any) => {
     return [contador, incrementar];
 };
 
-const Interval = (props: any) => {
+/* const Interval = (props: any) => {
     const { contador } = props;
 
     useEffect(() => {
         const i = setInterval(() => console.log(contador), 1000);
+
         return () => clearInterval(i);
     }, [contador]);
 
     return <p>Intervalo</p>;
-};
+}; */
+interface IntervalProps {
+    contador: number;
+}
+class Interval extends Component<IntervalProps> {
+    constructor(props: IntervalProps) {
+        super(props);
+    }
+
+    intervalo: any = '';
+
+    componentDidMount() {
+        this.intervalo = setInterval(
+            () => console.log(this.props.contador),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.intervalo);
+    }
+
+    render() {
+        return <p>Intervalo</p>;
+    }
+}
 
 const App = () => {
     const [contador, incrementar] = useContador(0);
