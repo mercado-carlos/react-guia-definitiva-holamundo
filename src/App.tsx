083b5488b5d9702b935e4b3fdd950d2594a257db
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-28 12:46:44 -0600

    Agregando elementos al carro parte 2

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,31 @@ class App extends React.Component {
             { name: 'Lechuga', price: 500, img: '/img/lechuga.jpg' },
         ],
         carro: [
-            /* {
+            {
                 name: 'Tomate',
                 price: 1500,
                 img: '/img/tomate.jpg',
-                cantidad: 1,
-            }, */
+                cantidad: 0,
+            },
         ],
     };
 
     agregarAlCarro = (producto: any) => {
+        const { carro } = this.state;
+
+        if (carro.find((x) => x.name === producto.name)) {
+            const newCarro = carro.map((x) =>
+                x.name === producto.name
+                    ? {
+                          ...x,
+                          cantidad: x.cantidad + 1,
+                      }
+                    : x
+            );
+
+            return this.setState({ carro: newCarro });
+        }
+
         return this.setState({
             carro: this.state.carro.concat({
                 ...producto,
