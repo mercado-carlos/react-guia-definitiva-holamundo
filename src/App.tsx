56f8a5f4ba7393b3ade2d9bd54b0edabf5cc5caf
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-30 13:31:39 -0600

    Llamando API

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css';
 import { Field, Form, Formik } from 'formik';
 
 const App = () => {
+    const [photos, setPhotos] = useState([]);
+
+    console.log({ photos });
+
     return (
         <div>
             <header>
                 <Formik
                     initialValues={{ search: '' }}
-                    onSubmit={async (values) => console.log(values)}
+                    onSubmit={async (values) => {
+                        const response = await fetch(
+                            `https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+                            {
+                                headers: {
+                                    Authorization:
+                                        'Client-ID ypI66F93L4z_w-zvxx9E4Mn4vXtpJhDGIuErfyLN_1I',
+                                },
+                            }
+                        );
+                        const data = await response.json();
+                        setPhotos(data.results);
+                    }}
                 >
                     <Form>
                         <Field name="search" />
