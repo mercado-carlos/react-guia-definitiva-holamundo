a0853104e7ec99e619f8a681bcf6862a9c1af24d
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-31 12:52:52 -0600

    React memo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import { useState } from 'react'
-import Title from './components/Title'
-import MyForm from './components/Forms/MyForm'
-import MyList from './components/Lists/MyList'
+import { useState } from 'react';
+import Title from './components/Title';
+import MyForm from './components/Forms/MyForm';
+import MyList from './components/Lists/MyList';
 
 function App() {
-  const [valores, setValores] = useState([])
-  const handleSubmit = (values) => {
-    setValores([
-      ...valores,
-      values
-    ])
-  }
+    const [valores, setValores] = useState([]);
+    const handleSubmit = (values) => {
+        setValores([...valores, values]);
+    };
 
-  return (
-    <div>
-      <Title>Mi título</Title>
-      <MyForm onSubmit={handleSubmit} />
-      <MyList data={valores} />
-    </div>
-  );
+    return (
+        <div>
+            <Title>Mi título</Title>
+            <MyForm onSubmit={handleSubmit} />
+            <MyList data={valores} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Lists/MyList.js
+++ src/components/Lists/MyList.js
@@ -1,24 +1,22 @@
-const Li = ({ fullname }) => {
-  console.log(`renderizando ${fullname}`)
-  return (
-    <li>
-      {fullname}
-    </li>
-  )
-}
+import { memo } from 'react';
+
+const Li = memo(({ fullname }) => {
+    console.log(`renderizando ${fullname}`);
+    return <li>{fullname}</li>;
+});
 
 const MyList = ({ data }) => {
-  console.log('renderizando lista')
-  return (
-    <ul>
-      {data.map(x =>
-        <Li
-          key={x.name + x.lastname}
-          fullname={`${x.name} ${x.lastname}`}
-        />
-      )}
-    </ul>
-  )
-}
+    console.log('renderizando lista');
+    return (
+        <ul>
+            {data.map((x) => (
+                <Li
+                    key={x.name + x.lastname}
+                    fullname={`${x.name} ${x.lastname}`}
+                />
+            ))}
+        </ul>
+    );
+};
 
-export default MyList
+export default MyList;
