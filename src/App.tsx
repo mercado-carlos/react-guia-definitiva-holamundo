ac208110e340d5ca550be5d18d2120f06ff01f7c
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-03-31 13:39:21 -0600

    Primeros pasos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { createContext, useContext } from 'react';
+
+const ContextDefault = createContext('valor por defecto');
+const Context2 = createContext('valor por defecto 2');
+
+const DefaultProvider = ({ children }: any) => {
+    return (
+        <ContextDefault.Provider value={'mi valor'}>
+            {children}
+        </ContextDefault.Provider>
+    );
+};
+
+const Contenido = () => {
+    const ctx = useContext(ContextDefault);
+
+    return <div>{ctx}</div>;
+};
+const Contenido2 = () => {
+    const ctx = useContext(Context2);
+
+    return <div>{ctx}</div>;
+};
 
 function App() {
-    return <div>Hola mundo</div>;
+    return (
+        <DefaultProvider>
+            <Contenido />
+            <Contenido2 />
+        </DefaultProvider>
+    );
 }
 
 export default App;
