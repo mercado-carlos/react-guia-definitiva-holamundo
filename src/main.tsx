3e2257e6989c0d0ef2f1e5529fa4788f603e92b5
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-04-01 09:28:41 -0600

    Primeros pasos

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { createStore } from 'redux';
+
+const store = createStore((state = 0, action) => {
+    switch (action.type) {
+        case 'accion': {
+            return action.payload;
+        }
+    }
+
+    return state;
+});
+
+store.dispatch({ type: 'accion', payload: 2 });
+store.dispatch({ type: 'accionindefinida', payload: 15 });
+console.log(store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
