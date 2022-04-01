d816939e86cbf1678e9f1997e32767bad54bae05
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-04-01 10:42:00 -0600

    Mas ejemplos con redux

--- src/main.tsx
+++ src/main.tsx
@@ -4,18 +4,30 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 
-const store = createStore((state = 0, action) => {
+const store = createStore((state: number = 0, action) => {
     switch (action.type) {
-        case 'accion': {
+        case 'incrementar': {
+            return state + 1;
+        }
+        case 'decrementar': {
+            return state - 1;
+        }
+        case 'set': {
             return action.payload;
         }
+        default:
+            return state;
     }
-
-    return state;
 });
 
-store.dispatch({ type: 'accion', payload: 2 });
-store.dispatch({ type: 'accionindefinida', payload: 15 });
+console.log(store.getState());
+store.dispatch({ type: 'lala' });
+console.log(store.getState());
+store.dispatch({ type: 'incrementar' });
+console.log(store.getState());
+store.dispatch({ type: 'decrementar' });
+console.log(store.getState());
+store.dispatch({ type: 'set', payload: 15 });
 console.log(store.getState());
 
 ReactDOM.render(
