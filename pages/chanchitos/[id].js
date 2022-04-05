64ed9770da20271f9ed281be87ca9912c40142d4
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-04-05 12:58:17 -0500

    useIsMounted

--- pages/chanchitos/[id].js
+++ pages/chanchitos/[id].js
@@ -1,17 +1,12 @@
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
+import useIsMounted from '../../hooks/useIsMounted';
+
 const ChanchitoDinamico = () => {
-    const [loaded, setLoaded] = useState(false);
+    const isMounted = useIsMounted();
     const router = useRouter();
 
-    useEffect(() => {
-        if (router.isReady) {
-            setLoaded(true);
-        }
-    }, [router.isReady]);
-
-    if (!loaded) {
+    if (!isMounted) {
         return null;
     }
 
