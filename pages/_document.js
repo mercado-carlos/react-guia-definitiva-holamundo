4a3c40e2aff77068b9b886af7218093560a53aa2
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-04-05 14:11:54 -0500

    Paginas estaticas dinamicas

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
--- pages/pokemones/[id].js
+++ pages/pokemones/[id].js
@@ -1,8 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Pokemon = ({ data }) => {
-    console.log(data);
+    const router = useRouter();
+    console.log(router);
+
     return (
         <div>
             <h1>
@@ -16,7 +19,7 @@ const Pokemon = ({ data }) => {
 
 export default Pokemon;
 
-export const getServerSideProps = async ({ params }) => {
+export const getStaticProps = async ({ params }) => {
     const response = await fetch(
         `https://pokeapi.co/api/v2/pokemon/${params.id}`
     );
@@ -24,3 +27,21 @@ export const getServerSideProps = async ({ params }) => {
 
     return { props: { data } };
 };
+
+export const getStaticPaths = async () => {
+    const paths = [{ params: { id: '1' } }, { params: { id: '2' } }];
+
+    return {
+        paths,
+        fallback: 'blocking',
+    };
+};
+
+/* export const getServerSideProps = async ({ params }) => {
+    const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${params.id}`
+    );
+    const data = await response.json();
+
+    return { props: { data } };
+}; */
