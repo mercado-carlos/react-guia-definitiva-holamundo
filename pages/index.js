7128fcf553a5cdb8e1c956cc45586d551c1b0235
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-04-05 13:09:56 -0500

    Componente image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
+import Tomate from '../public/tomate.jpg';
+
+// <Image src="/tomate.jpg" width={400} height={400} />
 export default function Home() {
     return (
         <div>
             <Link href="/chanchitos">Ir a chanchitos</Link>
             <p>Hola mundo</p>
+            <Image src={Tomate} width={400} height={400} />
         </div>
     );
 }
