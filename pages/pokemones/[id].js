25326ba18774c79dc2f7c5f46a58530d63412e90
mercado-carlos <carlos-roberto.mercado@atos.net>
2022-04-05 13:45:15 -0500

    Componente pokemon e imagenes

---
+++ pages/pokemones/[id].js
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const Pokemon = ({ data }) => {
+    console.log(data);
+    return (
+        <div>
+            <h1>
+                {data.name} numero #{data.id}
+            </h1>
+            <Image src={data.sprites.front_default} width={400} height={400} />
+            <Link href="/">Volver al inicio</Link>
+        </div>
+    );
+};
+
+export default Pokemon;
+
+export const getServerSideProps = async ({ params }) => {
+    const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${params.id}`
+    );
+    const data = await response.json();
+
+    return { props: { data } };
+};
--- pages/pokemones/[id]/index.js
+++
@@ -1,5 +0,0 @@
-const Index = () => {
-    return <p>Soy index</p>;
-};
-
-export default Index;
